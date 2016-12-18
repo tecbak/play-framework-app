d305543057533917bbf290526c98df5191cf7934
Dmitriy <dmytro_rud@epam.com>
2016-12-19 00:36:52 +0200

    Initial commit

---
+++ app/controllers/MyController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class MyController extends Controller {
+
+    public Result hello() {
+        return ok("Hello, world! :)");
+    }
+}
